02e2501e388fdda8a3fb2fabdfa87ac6eae5533b
Mike Barlow <mike@mikebarlow.co.uk>
2022-09-01 21:51:04 +0100

    fixing views

--- resources/views/announcements.blade.php
+++ resources/views/announcements.blade.php
@@ -1,6 +1,6 @@
 <div class="mb-announcements">
     <div class="relative w-12 h-12" x-data="{ open: false }">
-        @include('announcements::icon')
-        @include('announcements::popout')
+        @include('megaphone::icon')
+        @include('megaphone::popout')
     </div>
 </div>
