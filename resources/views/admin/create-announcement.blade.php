b323ed432df64679351ed5a0e7e5bd80bea905eb
Mike Barlow <mike@mikebarlow.co.uk>
2023-09-10 22:45:42 +0100

    Updating php unit config. Fixing tests

--- resources/views/admin/create-announcement.blade.php
+++ resources/views/admin/create-announcement.blade.php
@@ -12,7 +12,7 @@
         <div class="mb-6">
             <label for="type" class="block mb-2 text-sm font-medium text-gray-900">{{ __('Type') }}*</label>
             <select id="type" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5
-                @error('type') border-red-500 @enderror" wire:model="type">
+                @error('type') border-red-500 @enderror" wire:model.live="type">
                 <option>{{ __('Select Type') }}</option>
                 @foreach ($notifTypes as $type => $name)
                     <option value="{{ $type }}">
