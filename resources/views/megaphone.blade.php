867998928eaa2dde64ae1213b9d3a7a54f9d6d88
titonova <41286822+titonova@users.noreply.github.com>
2023-12-08 16:41:21 +0100

    Ensure the popout is always on top
    This fixes a bug where if your navbar is a fixed navbar the popout goes under the body and becomes invisible.
    
    Livewire v3 introduced @teleport that fixes the problem conveniently.
    
    https://livewire.laravel.com/docs/teleport

--- resources/views/megaphone.blade.php
+++ resources/views/megaphone.blade.php
@@ -1,6 +1,8 @@
 <div class="megaphone">
     <div class="relative w-12 h-12" x-data="{ open: false }">
         @include('megaphone::icon')
-        @include('megaphone::popout')
+        @teleport('body')
+            @include('megaphone::popout')
+        @endteleport
     </div>
 </div>
