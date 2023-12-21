6949107dad1e6ee3c412b5e5f98730765066abcf
Tito Amoo <titoamoo4@gmail.com>
2023-12-21 01:23:53 +0100

    Moved to use wireui/heroicons for more consistent, standard looking iconset

--- resources/views/popout.blade.php
+++ resources/views/popout.blade.php
@@ -5,12 +5,8 @@
         <div class="fixed top-0 left-0 z-0 w-full h-full" @click="open = false"></div>
         <div class="2xl:w-4/12 sm:w-2/5 bg-gray-50 dark:bg-slate-800 absolute right-0 z-30 w-full h-screen p-4 pt-8 overflow-y-auto shadow-md">
 
-            <button role="button" aria-label="close modal" class="focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500 rounded-md cursor-pointer" @click="open = false">
-                <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M18 6L6 18" stroke="#4B5563" stroke-width="1.25" stroke-linecap="round" stroke-linejoin="round" />
-                    <path d="M6 6L18 18" stroke="#4B5563" stroke-width="1.25" stroke-linecap="round" stroke-linejoin="round" />
-                </svg>
-            </button>
+            <button class="right-0" role="button" aria-label="close modal" @click="open = false"><x-icon name="x-mark" class="w-6 h-6 text-gray-500" /></button>
+
 
             <div class="flex items-center justify-between">
                 <p tabindex="0" class="focus:outline-none dark:text-gray-400 text-2xl font-semibold leading-6 text-gray-800">Notifications</p>
@@ -23,62 +19,61 @@
                 <hr class="w-full">
             </div>
 
-            <div x-show="unreadCount > 0" >
-                <div class="dark:border-slate-600 flex justify-between pt-8 pb-2 border-b border-gray-300">
-                    <h2 tabindex="0" class="focus:outline-none dark:text-slate-300 text-sm leading-normal text-gray-600">
-                        Unread Notifications
-                    </h2>
+            <div x-cloak x-show="!isShowingFilterSection">
+                <div x-show="unreadCount > 0" >
+                    <div class="dark:border-slate-600 flex justify-between pt-8 pb-2 border-b border-gray-300">
+                        <h2 tabindex="0" class="focus:outline-none dark:text-slate-300 text-sm leading-normal text-gray-600">
+                            Unread Notifications
+                        </h2>
 
-                    <!----- Mark all as read button ----->
-                    <h2 x-on:click="markAllAsRead" wire-id="{{ $this->getId() }}" tabindex="0" class="focus:outline-none dark:text-slate-400 float-right text-xs text-sm leading-normal text-gray-500">
-                        Mark all as read
-                    </h2>
+                        <!----- Mark all as read button ----->
+                        <h2 x-on:click="markAllAsRead" wire-id="{{ $this->getId() }}" tabindex="0" class="focus:outline-none dark:text-slate-400 float-right text-xs text-sm leading-normal text-gray-500">
+                            Mark all as read
+                        </h2>
+                    </div>
+
+                    <!----- Unread Notifications ----->
+                    @foreach ($unread as $announcement)
+                        @php $id = $announcement->id; @endphp
+                        <div id="unread-notification-{{ $id }}">
+                            <div class="w-full p-3 mt-4 bg-white  dark:bg-slate-900 border-none  rounded flex flex-shrink-0 {{ $announcement->read_at === null ? "drop-shadow shadow border" : ""  }}">
+                                <x-megaphone::display :notification="$announcement"></x-megaphone::display>
+
+                                @if($announcement->read_at === null)
+                                    <button role="button" aria-label="Mark as Read" class="focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500 w-6 h-6 rounded-md cursor-pointer"
+                                            x-on:click="markAsRead('{{ $id }}')"
+
+                                    >
+                                    <x-icon name="x-mark" class="w-6 h-6 text-gray-500" />
+                                    </button>
+                                @endif
+                            </div>
+                        </div>
+                    @endforeach
                 </div>
 
-                <!----- Unread Notifications ----->
-                @foreach ($unread as $announcement)
-                    @php $id = $announcement->id; @endphp
-                    <div id="unread-notification-{{ $id }}">
-                        <div class="w-full p-3 mt-4 bg-white  dark:bg-slate-900 border-none  rounded flex flex-shrink-0 {{ $announcement->read_at === null ? "drop-shadow shadow border" : ""  }}">
-                            <x-megaphone::display :notification="$announcement"></x-megaphone::display>
-
-                            @if($announcement->read_at === null)
-                                <button role="button" aria-label="Mark as Read" class="focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500 w-6 h-6 rounded-md cursor-pointer"
-                                        x-on:click="markAsRead('{{ $id }}')"
-
-                                >
-                                    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                        <path d="M18 6L6 18" stroke="#4B5563" stroke-width="1.25" stroke-linecap="round" stroke-linejoin="round" />
-                                        <path d="M6 6L18 18" stroke="#4B5563" stroke-width="1.25" stroke-linecap="round" stroke-linejoin="round" />
-                                    </svg>
-                                </button>
-                            @endif
-                        </div>
+                <!--------------- Previous Notifications ----------------->
+                @if ($announcements->count() > 0)
+                    <h2 tabindex="0" class="focus:outline-none dark:border-slate-600 dark:text-slate-300 pt-8 pb-2 text-sm leading-normal text-gray-600 border-b border-gray-300">
+                        Previous Notifications
+                    </h2>
+                @endif
+                @foreach ($announcements as $announcement)
+                    <div class="w-full p-3   dark:bg-slate-800 dark:border-slate-700 border-b border-gray-300 rounded flex flex-shrink-0 {{ $announcement->read_at === null ? "drop-shadow shadow border" : ""  }}">
+                        <x-megaphone::display :notification="$announcement"></x-megaphone::display>
                     </div>
                 @endforeach
+
+                @if ($unread->count() === 0 && $announcements->count() === 0)
+                    <div class="flex items-center justify-between">
+                        <hr class="w-full">
+                        <p tabindex="0" class="focus:outline-none flex flex-shrink-0 px-3 py-16 text-sm leading-normal text-gray-500">
+                            No new announcements
+                        </p>
+                        <hr class="w-full">
+                    </div>
+                @endif
             </div>
-
-            <!--------------- Previous Notifications ----------------->
-            @if ($announcements->count() > 0)
-                <h2 tabindex="0" class="focus:outline-none dark:border-slate-600 dark:text-slate-300 pt-8 pb-2 text-sm leading-normal text-gray-600 border-b border-gray-300">
-                    Previous Notifications
-                </h2>
-            @endif
-            @foreach ($announcements as $announcement)
-                <div class="w-full p-3   dark:bg-slate-800 dark:border-slate-700 border-b border-gray-300 rounded flex flex-shrink-0 {{ $announcement->read_at === null ? "drop-shadow shadow border" : ""  }}">
-                    <x-megaphone::display :notification="$announcement"></x-megaphone::display>
-                </div>
-            @endforeach
-
-            @if ($unread->count() === 0 && $announcements->count() === 0)
-                <div class="flex items-center justify-between">
-                    <hr class="w-full">
-                    <p tabindex="0" class="focus:outline-none flex flex-shrink-0 px-3 py-16 text-sm leading-normal text-gray-500">
-                        No new announcements
-                    </p>
-                    <hr class="w-full">
-                </div>
-            @endif
 
         </div>
 
